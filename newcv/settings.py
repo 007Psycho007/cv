77c8a61822ca3ae56699766ec69d5dc70f6def2b
Jan <lp.forever007@yahoo.de>
2022-03-15 15:29:41 +0100

    Update settings.py

--- newcv/settings.py
+++ newcv/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$)+j!hshj9a-#+*!wx+09z#$6-ci7$wlsuvsm6c5uw&^=ku3qr'
+SECRET_KEY = config.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
